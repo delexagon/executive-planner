f5c2367573ca645727623b4628e00c495d11b36f
Zach Love <abc811@gmail.com>
2022-04-04 22:35:58 -0400

    Created unit test structure for Calendar and Search; Created rudimentary unit tests for Event and EventList objects; Replaced built-in widget test with a functional unit test

---
+++ test/calendar_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:executive_planner/pages/calendar.dart';
+
+void main() {
+  // Test Calendar Constructor
+
+  // Test Navigation to Calendar view
+
+  // Test User Input (change date range)
+
+  // Test User Input (select day)
+
+  // Test focus selection
+
+}
