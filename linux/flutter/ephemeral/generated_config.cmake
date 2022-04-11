8484bfd84b0d0790bdadeff3268efe64617a3f16
delexagon <delexagon@gmail.com>
2022-04-11 01:33:41 -0400

    Fixed analysis issues

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/delexagon/Downloads/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/delexagon/Downloads/executive-planner-development/executive_planner" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/delexagon/Downloads/flutter"
+  "PROJECT_DIR=/home/delexagon/Downloads/executive-planner-development/executive_planner"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/delexagon/Downloads/executive-planner-development/executive_planner/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/delexagon/Downloads/executive-planner-development/executive_planner/lib/main.dart"
+)
